a4c0d2d00dc733bbe508600514805bd75640482b
Max Base <MaxBaseCode@Gmail.Com>
2018-12-21 22:00:15 +0330

    Update fix.rs

--- fix.rs
+++ fix.rs
@@ -2,49 +2,57 @@ fn main()
 {
 	//This code is not correct!
 	let input="(4*2";
-	let mut bracket_round:i32=0;
-	let mut bracket_curly:i32=0;
-	let mut bracket_square:i32=0;
+	let mut bracket:[i32;500];
+	let mut bracket_index=0;
+	// let mut bracket_round:i32=0;
+	// let mut bracket_curly:i32=0;
+	// let mut bracket_square:i32=0;
 	for c in input.chars()
 	{
 		if c == '{'
 		{
-			bracket_curly+=1;
+			bracket_index+=1;
+			// bracket_curly+=1;
 		}
 		else if c == '['
 		{
-			bracket_square+=1;
+			bracket_index+=1;
+			// bracket_square+=1;
 		}
 		else if c == '('
 		{
-			bracket_round+=1;
+			bracket_index+=1;
+			// bracket_round+=1;
 		}
 		/////////////////////
 		else if c == '}'
 		{
-			bracket_curly-=1;
+			bracket_index-=1;
+			// bracket_curly-=1;
 		}
 		else if c == ']'
 		{
-			bracket_square-=1;
+			bracket_index-=1;
+			// bracket_square-=1;
 		}
 		else if c == ')'
 		{
-			bracket_round-=1;
+			bracket_index-=1;
+			// bracket_round-=1;
 		}
 		/////////////////////
 		println!("{}",c);
 	}
-	if(bracket_round !== 0)
-	{
+	// if bracket_round != 0
+	// {
 		
-	}
-	if(bracket_curly !== 0)
-	{
+	// }
+	// if bracket_curly != 0
+	// {
 		
-	}
-	if(bracket_square !== 0)
-	{
+	// }
+	// if bracket_square != 0
+	// {
 
-	}
+	// }
 }
